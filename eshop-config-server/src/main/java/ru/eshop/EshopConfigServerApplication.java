9a032881bc873319d61eb0abbfabe163e5ca9267
AlexandrSilin <67175@mail.ru>
2021-10-06 11:44:56 +0300

    Add config server

---
+++ eshop-config-server/src/main/java/ru/eshop/EshopConfigServerApplication.java
@@ -0,0 +1,16 @@
+package ru.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class EshopConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopConfigServerApplication.class, args);
+    }
+
+}

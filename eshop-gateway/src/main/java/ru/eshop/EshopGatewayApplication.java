1c7bd8dc106f7b4137ff1f8a65971cf7b6840521
AlexandrSilin <67175@mail.ru>
2021-10-06 14:17:59 +0300

    Complete 7 homework

--- eshop-gateway/src/main/java/ru/eshop/EshopGatewayApplication.java
+++ eshop-gateway/src/main/java/ru/eshop/EshopGatewayApplication.java
@@ -2,14 +2,14 @@ package ru.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
-public class EshopConfigServerApplication {
+public class EshopGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EshopConfigServerApplication.class, args);
+        SpringApplication.run(EshopGatewayApplication.class, args);
     }
 
 }
--- picture-service/src/main/java/ru/eshop/PictureServiceApplication.java
+++ picture-service/src/main/java/ru/eshop/PictureServiceApplication.java
@@ -2,14 +2,10 @@ package ru.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
-public class EshopConfigServerApplication {
-
+public class PictureServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EshopConfigServerApplication.class, args);
+        SpringApplication.run(PictureServiceApplication.class, args);
     }
-
 }
